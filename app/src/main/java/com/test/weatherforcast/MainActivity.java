762569235646b95bdb9b9b955d9c526dbe494517
runoob <1048761213@qq.com>
2020-01-13 15:22:30 +0800

    init

---
+++ app/src/main/java/com/test/weatherforcast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.test.weatherforcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(prefs.getString("weather",null) != null){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+//创建数据库用的
+//        Button createDatabase = findViewById(R.id.Create_DataBase);
+//        createDatabase.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Connector.getDatabase();
+//            }
+//        });
+    }
+}
